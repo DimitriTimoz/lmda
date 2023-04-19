b71d50244b15dc4e59d1b1987e8bca076bca521e
Dimitri <dimitri.timoz@orange.fr>
2023-04-19 19:45:24 +0200

    init project

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const app = express();
 
+// Serve the static files from the React app
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 const bodyParser = require('body-parser');
 const cors = require('cors');
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-  });
-
-  
-app.listen(5001, () => {
-    console.log('TODO: Settings for the server');
-    console.log('Server started on port 5001');
+// Endpoint for handling API requests
+app.get('/api', (req, res) => {
+    res.send('API endpoint');
 });
+  
+// Handles any requests that don't match the API endpoint
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
+  
+  
+const port = process.env.PORT || 5001;
+app.listen(port);
 
+console.log(`App is listening on port ${port}`);
