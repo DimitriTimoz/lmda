9b461b390cbbaf92c648d11112a2054dc7d5b605
Dimitri <dimitri.timoz@orange.fr>
2023-04-21 16:25:09 +0200

    product basic info

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ImageViews from "../components/ImageViews";
+import Button from "../components/Button";
+import LikeBtn from "../components/LikeBtn";
+
+import "./Product.css";
 
 export default function Product() {
     const product_id = window.location.pathname.split("/")[2];
@@ -7,6 +11,7 @@ export default function Product() {
         photos: ["/images/clothes/main_1.png", "/images/clothes/second_1_1.png", "/images/clothes/second_2_1.png", "/images/clothes/second_2_1.png"],
         name: "T-shirt",
         size: "M",
+        description: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
         price: 20,
         id: 1
     }
@@ -15,9 +20,20 @@ export default function Product() {
 
     return (
         <div>
-        <p>Product</p>
-        <p>{product_id}</p>
-        <ImageViews photos={product.photos}/>
+            <div className="product">
+                <ImageViews photos={product.photos}/>
+                <div className="product-block">
+                    <h2>{product.name}</h2>
+                    <span className="price">{product.price} €</span>
+                    <span className="product-description">
+                        {product.description}
+                    </span>
+                    <Button title="Acheter" className="" onClick="alert(1)" />
+                    <LikeBtn product={product} isFavorite={false} onToggleFavorite={()=>{}} />
+                    <div className="product-details">
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
