9e68934da1c6264dfcec5c153d0aebc2c0c97397
Dimitri <dimitri.timoz@orange.fr>
2023-04-21 15:37:25 +0200

    same

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -1,12 +1,23 @@
 import React from "react";
+import ImageViews from "../components/ImageViews";
 
 export default function Product() {
     const product_id = window.location.pathname.split("/")[2];
-  
+    let product = {
+        photos: ["/images/clothes/main_1.png", "/images/clothes/second_1_1.png", "/images/clothes/second_2_1.png", "/images/clothes/second_2_1.png"],
+        name: "T-shirt",
+        size: "M",
+        price: 20,
+        id: 1
+    }
+
+
+
     return (
         <div>
         <p>Product</p>
         <p>{product_id}</p>
+        <ImageViews photos={product.photos}/>
         </div>
     );
 }
