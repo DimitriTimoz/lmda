eaa74931f9484c3a578236295275252092fb77b2
Dimitri <dimitri.timoz@orange.fr>
2023-06-29 14:08:06 +0200

    orders db

---
+++ server/migrations/1687970064783_orders.js
@@ -0,0 +1,63 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+    pgm.createTable("users", {
+        id: {
+            type: "serial",
+            primaryKey: true,
+            unique: true
+        },
+        email : {
+            type: "varchar(255)",
+            notNull: true,
+        },
+        phone : {
+            type: "varchar(255)",
+            notNull: true,
+        },
+        address : {
+            type: "text",
+            notNull: true,
+        },
+    });
+
+    pgm.createTable("orders", {
+        id: {
+            type: "serial",
+            primaryKey: true,
+            unique: true
+        },
+        user_id: {
+            type: "integer",
+            notNull: true,
+        },
+        products: {
+            type: "integer[]",
+            notNull: true,
+        },
+        date: {
+            type: "timestamp",
+            notNull: true,
+        },
+        total: {
+            type: "integer",
+            notNull: true,
+        },
+        status: {
+            type: "integer",
+            notNull: true,
+        },
+        address: {
+            type: "text",
+            notNull: true,
+        },
+    });
+
+};
+
+exports.down = pgm => {
+    pgm.dropTable("users");
+    pgm.dropTable("orders");
+};
