b77ca6423c4ecc0b60b01e52fa0489cdb2a3bce9
Dimitri <dimitri.timoz@orange.fr>
2023-08-08 17:06:10 +0200

    new Carrousel

--- client/src/components/products/ImageCarrousel.js
+++ client/src/components/products/ImageCarrousel.js
@@ -11,36 +11,26 @@ export default class ImageCarrousel extends React.Component {
 
         this.nextPhoto = this.nextPhoto.bind(this);
         this.prevPhoto = this.prevPhoto.bind(this);
-        this.closeCarrousel = this.closeCarrousel.bind(this);
     }
 
     componentDidMount() {
         this.setState({ photos: this.props.photos });
     }
 
-    selectPhoto(index) {
-        this.setState({
-            selected: index,
-        });
-    }
-
-    closeCarrousel() {
-        this.props.enabled = false;
-    }
-
     nextPhoto() {
-        let selected = this.state.selected;
-        let photos = this.state.photos;
-        if (selected < photos.length - 1) {
-            this.setState({ selected: selected + 1 });
-        }
+        const slidesContainer = document.getElementById("slides-container");
+        const slide = document.querySelector(".slide");
+        const slideWidth = slide.clientWidth;
+        slidesContainer.scrollLeft += slideWidth;
+
     }
 
     prevPhoto() {
-        let selected = this.state.selected;
-        if (selected > 0) {
-            this.setState({ selected: selected - 1 });
-        }
+        const slidesContainer = document.getElementById("slides-container");
+        const slide = document.querySelector(".slide");
+        const slideWidth = slide.clientWidth;
+        slidesContainer.scrollLeft -= slideWidth;
+
     }
 
     render() {
@@ -49,27 +39,26 @@ export default class ImageCarrousel extends React.Component {
         if (this.props.enabled === false) {
             return <div></div>;
         }
+  
         return (
-            <div className="image-carrousel">
-                <div className="image-carrousel-main">
-                    <img src={selected_photo} className="image-carrousel-main-image" alt="Prévisualisation du vêtement principale"/>
-                    <div className="image-carrousel-buttons">
-                        <button className="image-carrousel-button" onClick={this.prevPhoto}>{"<"}</button>
-                        <button className="image-carrousel-button" onClick={this.nextPhoto}>{">"}</button>
-                    </div>
-                </div>
-                <div className="image-carrousel-thumbnails">
-                    {this.state.photos.map((photo, index) => {
+            <section class="slider-wrapper">
+                <button className="slide-arrow" id="slide-arrow-prev" onClick={this.prevPhoto}>
+                    &#8249;
+                </button>
+                <button className="slide-arrow" id="slide-arrow-next" onClick={this.nextPhoto}>
+                    &#8250;
+                </button>
+                <ul className="slides-container" id="slides-container">
+                {this.state.photos.map((photo, index) => {
                         if (index === selected) {
-                            return <img src={photo} className="image-carrousel-thumbnail selected" alt="Prévisualisation du vêtement selectionnée" onClick={() => this.selectPhoto(index)}/>
+                            return <li className="slide"><img src={photo} className="image-carrousel" alt="Prévisualisation du vêtement selectionnée" onClick={() => this.selectPhoto(index)}/></li>
                         } else {
-                            return <img src={photo} className="image-carrousel-thumbnail" alt="Prévisualisation selectionnable" onClick={() => this.selectPhoto(index)}/>
+                            return <li className="slide"><img src={photo} className="image-carrousel" alt="Prévisualisation selectionnable" onClick={() => this.selectPhoto(index)}/></li>
                         }
                     }
-                    )}
-                </div>
-                
-            </div>
+                )}
+                </ul>
+            </section>
         );
     }
 }
