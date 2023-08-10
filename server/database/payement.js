b7b3ec8f11f3bd83daf74ed29e82b30920ca99c5
Dimitri <dimitri.timoz@orange.fr>
2023-08-10 15:15:14 +0200

    cancler payement

--- server/database/payement.js
+++ server/database/payement.js
@@ -1,5 +1,6 @@
 const db = require('../db');
 
+// Update the order to mark it as paid
 async function valid_payement(stripe_id) {
     try {
         const query = `UPDATE orders SET paid = TRUE WHERE stripe_id = $1 `;
@@ -11,6 +12,28 @@ async function valid_payement(stripe_id) {
     }
 }
 
+// Update the order to mark it as canceled and make the products available again
+async function payement_canceled(stripe_id) {
+    try {
+        const query = `UPDATE orders SET paid = FALSE WHERE stripe_id = $1 `;
+        const { rows } = await db.query(query, [stripe_id]);
+        if (rows.length !== 1) {
+            return false;
+        }
+        const order = rows[0];
+        const products = order.products;
+        for (let i = 0; i < products.length; i++) {
+            await db.query('UPDATE products SET ordered = FALSE WHERE id = $1', [products[i]]);
+        }
+        return true;
+    } catch (error) {
+        console.error('Error getting images:', error.message);
+        return false;
+    }
+}
+        
+
 module.exports = {
-    valid_payement
+    valid_payement,
+    payement_canceled
 };
