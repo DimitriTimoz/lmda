12d293c6ad7a130d8483e47092129e3e9bd55964
Dimitri <dimitri.timoz@orange.fr>
2023-08-12 21:27:54 +0200

    test widget

---
+++ client/public/scripts/ralay.js
@@ -0,0 +1,11 @@
+!function(){
+	// Parameterized the widget
+  $(document).ready(function () {  
+    $("#Zone_Widget").MR_ParcelShopPicker({  
+      Target: "#ParcelShopCode",
+      Brand: "BDTEST  ",
+      Country: "FR" ,
+      Responsive: true,  
+    });  
+  });
+}();
