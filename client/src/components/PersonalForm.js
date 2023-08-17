2655ffa89bbf2fe52715ea0b7bc5314dc5f9f99b
DimitriTimoz <dimitri.timoz@orange.fr>
2023-08-17 20:48:35 +0200

    personal info form

---
+++ client/src/components/PersonalForm.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Input  from "./Input"
+import "./PersonalForm.css";
+
+export default function PersonalForm(props) {
+    const [infos, setInfos] = React.useState(props.infos || {});
+
+    const updateField = (e) => {
+        setInfos({
+            ...props.infos,
+            [e.target.name]: e.target.value,
+        });
+
+        if (props.setInfos) {
+            props.setInfos({
+                ...props.infos,
+                [e.target.name]: e.target.value,
+            });
+        }
+    }
+    
+    return (
+        <div className="infos-form">
+            <label class="full-field">
+                <span class="form-label">Nom Prénom</span>
+                <Input
+                    name="name"
+                    type="text"
+                    required
+                    autocomplete="on"
+                    placeholder="Nom Prénom.."
+                    onChange={updateField}
+                    value={infos.name}
+                />
+            </label>
+            <label class="full-field">
+                <span class="form-label">Téléphone</span>
+                <Input id="tel" name="tel" type="tel" onChange={updateField} placeholder="06 00 00 00 00" value={infos.tel} />
+            </label>
+            <label class="full-field">
+                <span class="form-label">Email</span>
+                <Input id="email" name="email" type="email" onChange={updateField} required placeholder="nom.prenom@example.com" value={infos.email}/>
+            </label>
+            
+      </div>  
+    );
+}
