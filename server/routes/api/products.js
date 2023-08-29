edef332fc97a7597e01d40aebf2d91842af99fa4
DimitriTimoz <dimitri.timoz@orange.fr>
2023-08-29 15:10:40 +0200

    filter with backend

--- server/routes/api/products.js
+++ server/routes/api/products.js
@@ -1,13 +1,19 @@
 var router = require('express').Router();
 const { getImagesFilenames } = require('../../database/images');
 const pool = require('../../db');
+const validator = require('validator');
 
-const selectAll = async (admin, kind, from = 0, more = 10) => {
+const selectAll = async (kind, filter, from = 0, more = 10) => {
   // Get all products
   let kindCondition = '';
   let values = [];
   if (kind !== 'all') {
     kindCondition = `AND kind = '${kind}'`;
+  }
+
+  if (filter !== 'all') {
+    // Start with filter
+    kindCondition += ` AND ("specifyCategory" LIKE '${filter}%' OR "specifyCategory" = '${filter}') `;
   }
   let queryString = `SELECT * FROM products WHERE (ordered = false ` + kindCondition + `) ORDER BY date DESC OFFSET $1 LIMIT $2;`;
   values = [from, more];
@@ -49,35 +55,13 @@ function applyFilter(products, category, filter) {
 router.get('/:category/:filter', async (req, res, next) => {
   const {from, more} = req.query;
   let category = req.params.category;
+  let filter = validator.escape(req.params.filter.toLowerCase().split(" ").join("-"));
   // if end with 's' remove it
   if (category[category.length - 1] === 's') {
     category = category.slice(0, -1);
   }
-  result = await selectAll(false, category, from, more);
+  result = await selectAll(category, filter, from, more);
   return res.json({products: result});  
 });
-
-
-router.get('/admin/:category/:filter', async (req, res, next) => {
-  // Check that the user is logged in
-  if (!req.session.loggedin) {
-    return res.status(401).json({ error: 'Vous devez être connecté pour récupérer les produits en tant qu\'administrateur.' });
-  } 
-
-  let category = req.params.category;
-
-  // if end with 's' remove it
-  if (category[category.length - 1] === 's') {
-    category = category.slice(0, -1);
-  }
-
-  // Get all products as an admin
-  result = await selectAll(true);
-  result = applyFilter(result, category, req.params.filter);
-  
-  return res.json({products: result});
-});
-
-
 
 module.exports = router;
